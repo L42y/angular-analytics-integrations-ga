a3617a092f0bd5e0d1770c7e18d52d898158cb80
L42y <423300@gmail.com>
2014-08-04 18:27:11 +0800

    init: initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# [angular](https://angularjs.org)-[analytics](https://github.com/L42y/angular-analytics)-integrations-[ga](https://developers.google.com/analytics/devguides/collection/gajs/)
+
+## Installation
+
+1. `bower install --save L42y/angular-analytics-integrations-ga`
+
+2. including script file provided by this component into your application
+
+3. adding `l42y.analytics.integrations.ga` into your application
+
+## License
+
+[WTFPL](http://wtfpl.org)
---
+++ angular-analytics-integrations-ga.js
@@ -0,0 +1,36 @@
+angular.module('l42y.analytics.integrations.ga', [
+  'l42y.analytics'
+]).config(function (
+  AnalyticsProvider
+) {
+  var identifier = 'ga.js';
+  var variableName = '_gaq';
+
+  var config = AnalyticsProvider.config.integrations[identifier];
+  (function (window, document, script, variableName, config, firstScript) {
+    window[variableName] = [
+      ['_setAccount', config.accountId],
+      ['_setDomainName', config.domain || 'auto']
+    ];
+    var scriptElement = document.createElement(script);
+    scriptElement.src='//www.google-analytics.com/ga.js';
+    firstScript = document.getElementsByTagName(script)[0];
+    firstScript.parentNode.insertBefore(scriptElement, firstScript);
+  }(window, document, 'script', variableName, config));
+
+  AnalyticsProvider.integrate(identifier, {
+    page: function trackPageViewByGoogleAnalytics (current) {
+      window[variableName].push(['_trackPageview', current]);
+    },
+    track: function trackEventByGoogleAnalytics (event, prop) {
+      window[variableName].push([
+        '_trackEvent',
+        prop.category,
+        event,
+        prop.label,
+        prop.value,
+        prop.noninteraction
+      ]);
+    }
+  });
+});
